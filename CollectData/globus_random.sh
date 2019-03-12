f63491ea600a236da314f2af3fae1b89b34a8084
Christina Mao <cmao@ucdavis.edu>
2019-03-11 19:14:42 -0700

    Added to-dos to globus_random.sh

--- CollectData/globus_random.sh
+++ CollectData/globus_random.sh
@@ -4,21 +4,17 @@
 # DATE: 03-11-2018
 # DESCRIPTION:Run Globus transfers randomly between a period of 5 minutes to 1 hour 
 # Guide to run ESnet DTN: https://fasterdata.es.net/performance-testing/DTNs/	 
+# To-do: Not working for star, sunn dtns
+# To-do: Sleep randomly in a 1 hour interval
 #=================================================================================================
-# GLOBAL VARIABLES
-DATESTAMP=$(date +"%Y-%m-%d")
-TIMESTAMP=$(date +"%Y-%m-%d-%H:%M:%S")
-echo $TIMESTAMP $DATESTAMP $PWD
-
 
 #GLOBUS PARAMETERS
 DTNS=("//cern-dtn.es.net" "//newy-dtn.es.net" "//star-dtn.est.net" "//sunn-dtn.es.net") 
 DATASETS=("/data1/500G.dat" "/data1/100G.dat" "/data1/500M.dat" "/data1/100M")
 CLIMATEDATA=("/data1/Climate-Huge" "/data1/Climate-Large" "/data1/Climate-Medium" "/data1/Climate-Small") 
 
-DTN="//newy-dtn.es.net:2811/data1/10M.dat" 
 DEST="/home/cmao/share/"
-FLOWS="4"
+FLOWS=$(shuf -i 4-8 -n 1)
 PROTOCOL="ftp"
 
 #while true; do
@@ -26,13 +22,7 @@ PROTOCOL="ftp"
 	sleep 1s
 	i=$(( $RANDOM % ${#DTNS[@]}))
 	y=$(( $RANDOM % ${#DATASETS[@]}))
-	f=$(shuf -i 4-8 -n 1)
-	echo $f	
 	echo ${DTNS[i]}
-
-	#for i in ${DTNS[@]}; do 
-	#	echo $i
-	#done
 
 	# Start Globus transfer
 	globus-url-copy -vb -fast -p $FLOWS ftp:${DTNS[i]}:2811${DATASETS[y]} file:$DEST 
