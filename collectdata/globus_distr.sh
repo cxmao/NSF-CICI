390e1831400afe518875732e5a0cc5b067455c10
rkgegan <rkgegan@ucdavis.edu>
2019-04-03 13:44:34 -0700

    Update globus_distr.sh

--- collectdata/globus_distr.sh
+++ collectdata/globus_distr.sh
@@ -1,33 +1,35 @@
+
 #!/usr/bin/env bash
 #
 # AUTHOR: Christina Mao
-# DATE: 04-02-2018
-# DESCRIPTION:Run Globus transfers randomly according to a distribution
-# Each interval, a small, medium, or large file is transferred 
+# DATE: 03-11-2018
+# DESCRIPTION:Run Globus transfers randomly between a period of 5 minutes to 1 hour
+# Files are selected by small (up to 1G), medium (up to 50G), and large (up to 500G)
+# Files are pulled randomly according to a distribution
+# A new distribution is selected randomly each day
 # Guide to run ESnet DTN: https://fasterdata.es.net/performance-testing/DTNs/
-# To-do: Not working for star, newy dtns
-# To-do: Decide which files (only climate vs only GB.dat files vs mix)
-# To-do: remove files after writing
+# Not working for star, newy dtns
+# Sleeps randomly within a 1 hour interval
 #=================================================================================================
 
 #GLOBUS PARAMETERS
 DTNS=("//cern-dtn.es.net" "//sunn-dtn.es.net")
 DATASETS=("/data1/500G.dat" "/data1/100G.dat" "/data1/50G.dat" "/data1/10G.dat" "/data1/1G.dat"  "/data1/100M.dat")
 CLIMATEDATA=("/data1/Climate-Huge" "/data1/Climate-Large" "/data1/Climate-Medium" "/data1/Climate-Small")
-SMALLDATA=("//data1/5GB-in-small-files/" "//data1/5MB-in-tiny-files" "//data1/100M.dat" "//data1/1G.dat")
-MEDIUMDATA=("//data1/50GB-in-medium-files" "//data1/10G.dat")
-LARGEDATA=("//data1/500GB-in-large-files" "//data1/Climate-Large/" "//data1/100G.dat" "//data1/500G.dat" "/data1/Climate-Large/" "/data1/Climate-Small/")
+SMALLDATA=("//data1/10M.dat" "//data1/50M.dat" "//data1/100M.dat" "//data1/1G.dat")
+MEDIUMDATA=("//data1/10G.dat" "//data1/50G.dat")
+LARGEDATA=("//data1/100G.dat" "//data1/500G.dat" "/data1/Climate-Large/" "/data1/Climate-Small/")
 
 #Climate data on one day, otherwise use normal data files
 
 #PERCENTAGE RANGE TO SELECT FILE 1-100
 #if medium percentage is 60, and small is 30, 30-60 will trigger medium transfer
-SMALL_PERCENT=50
-MEDIUM_PERCENT=80
+SMALL_PERCENT=70
+MEDIUM_PERCENT=90
 LARGE_PERCENT=100
 
 #Time of day to clean the file transfer directory
-DELETE_TIME=13
+DELETE_TIME=23
 #Don't remove file more than once during that hour
 REMOVED_SWITCH=false
 
@@ -39,22 +41,48 @@ PROTOCOL="ftp"
 #TIME BETWEEN TRANSFERS IN SECONDS
 INTERVAL=3600
 
-#while true; do
+while true; do
         curr_time=$(date +"%H")
         rand_file=$(($RANDOM % 100 + 1))
-        echo $rand_file
-        if [[ $curr_time -eq $DELETE_TIME ]] && [[ $REMOVED_SWITCH -eq false ]]
+        interval=$(($RANDOM % 3540 + 60))
+        echo $interval "seconds until next transfer"
+        sleep $interval
+
+        echo $curr_time
+        echo $DELETE_TIME
+        echo $REMOVED_SWITCH
+        if [[ $curr_time -eq $DELETE_TIME ]] && ! $REMOVED_SWITCH
         then
                 echo "removing files"
                 rm -r -f "$DEST"*
-                REMOVED_SWITCH=$((true))
-        elif [[ $REMOVED_SWITCH -eq true ]] && [[ $curr_time -ne $DELETE_TIME ]]
+                REMOVED_SWITCH=true
+                echo $REMOVED_SWITCH
+        elif $REMOVED_SWITCH && [[ $curr_time -ne $DELETE_TIME ]]
         then
                 echo "new day"
-                REMOVED_SWITCH=$((false))
+                # Select a new distribution for this day
+                distribution=$(($RANDOM % 3 + 1))
+                if [[ $distribution -eq 1 ]]
+                then
+                        SMALL_PERCENT=70
+                        MEDIUM_PERCENT=90
+                        LARGE_PERCENT=100
+                elif [[ $distribution -eq 2 ]]
+                then
+                        SMALL_PERCENT=30
+                        MEDIUM_PERCENT=70
+                        LARGE_PERCENT=100
+                elif [[ $distribution -eq 3 ]]
+                then
+                        SMALL_PERCENT=20
+                        MEDIUM_PERCENT=50
+                        LARGE_PERCENT=100
+                fi
+                echo $SMALL_PERCENT "percent small files selected."
+                REMOVED_SWITCH=false
         fi
-        sleep_time=$(($RANDOM % $INTERVAL + 1))
-        sleep 1 #sleep $sleep_time
+        #sleep_time=$(($RANDOM % $INTERVAL + 1))
+        #sleep $sleep_time
         i=$(( $RANDOM % ${#DTNS[@]}))
         y=$(( $RANDOM % ${#DATASETS[@]}))
         s=$(( $RANDOM % ${#SMALLDATA[@]}))
@@ -82,8 +110,5 @@ INTERVAL=3600
         globus_pid=$!
         echo $globus_pid
         wait $globus_pid
-        echo "erasing files in " $DEST"*"
-        #run this at the end of the day
-        #rm -r -f "$DEST"*
         echo "Globus finished transfer"
-#done
+done
