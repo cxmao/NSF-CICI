30215e59c1a5fe6b8ea310cb1e6d9eb5dc8a7140
rkgegan <rkgegan@ucdavis.edu>
2019-04-16 15:37:15 -0700

    Create syn_flood.sh

---
+++ attack_scripts/syn_flood.sh
@@ -0,0 +1,13 @@
+# Setup the hping3 SYN flood experiment
+echo "/home/ross/testing/syn_12pm.sh" | sudo at 12pm tomorrow
+echo "/home/ross/testing/syn_2pm.sh" | sudo at 2pm tomorrow
+echo "/home/ross/testing/syn_4pm.sh" | sudo at 4pm tomorrow
+echo "/home/ross/testing/syn_6pm.sh" | sudo at 6pm tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 1am tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 4am tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 7am tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 10am tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 1pm tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 4pm tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 7pm tomorrow
+echo "iperf3 -c 10.40.1.1" | sudo at 10pm tomorrow
